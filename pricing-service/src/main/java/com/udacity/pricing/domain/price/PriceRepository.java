5d869a1b48a9130bae5ac1a22beba96b8105431c
AndyBeHere <imfangcong@gmail.com>
2020-01-05 10:42:09 +0800

    finish it

---
+++ pricing-service/src/main/java/com/udacity/pricing/domain/price/PriceRepository.java
@@ -0,0 +1,8 @@
+package com.udacity.pricing.domain.price;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+public interface PriceRepository extends CrudRepository<Price, Long> {
+
+}
---
+++ pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
@@ -0,0 +1,33 @@
+package com.udacity.pricing;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class PricingServiceApplicationTests {
+
+	@Autowired
+	private MockMvc mvc;
+
+	@Test
+	public void contextLoads() {
+
+	}
+
+	@Test
+	public void textExample() throws Exception {
+		mvc.perform(get("/services/price").param("vehicleId", "1"))
+					.andExpect(status().isOk());
+	}
+
+}
